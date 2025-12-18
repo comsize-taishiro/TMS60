51fd530ea16da5def1507fb9dce4c1f3d30de072
comsize-Shioya <yuka.shioya@comsize.com>
2025-12-18 11:07:28 +0900

    12/18 DAO,Servlet,jspの編集

--- src/main/java/model/dao/TaskEditDAO.java
+++ src/main/java/model/dao/TaskEditDAO.java
@@ -1,13 +1,14 @@
 package model.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import model.entity.TaskBean;
 
 public class TaskEditDAO {
-	public int UpdateItem(TaskBean task)
+	public int updateTask(TaskBean task)
 			throws SQLException, ClassNotFoundException, NumberFormatException, IllegalArgumentException {
 		String sql = "UPDATE t_task SET task_name = ?, "
 				+ "category_id = ?, limit_date = ?, "
@@ -17,12 +18,14 @@ public class TaskEditDAO {
 				PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setString(1, task.getTask_name());
 			pstmt.setInt(2, task.getCategory_id());
-			pstmt.setTimestamp(3, task.getLimit_date());
-			pstmt.setString(4, task.getMemo());
-			pstmt.setInt(5, task.getTask_id());
-			int count = pstmt.executeUpdate();
+			pstmt.setDate(3, Date.valueOf(task.getLimit_date()));
+			pstmt.setString(4, task.getUser_id());
+			pstmt.setString(5, task.getStatus_code());
+			pstmt.setString(6, task.getMemo());
+			pstmt.setInt(7, task.getTask_id());
+			int result = pstmt.executeUpdate();
 
-			return count;
+			return result;
 		}
 	}
 }
--- src/main/java/servlet/TaskListServlet.java
+++ src/main/java/servlet/TaskListServlet.java
@@ -1,12 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.TaskListDAO;
+import model.entity.TaskBean;
 
 /**
  * Servlet implementation class TaskListServlet
@@ -37,6 +43,23 @@ public class TaskListServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
+		
+		List<TaskBean> taskList = null;
+		TaskListDAO dao = new TaskListDAO();
+		
+		try {
+			taskList = dao.selectAll();
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		request.setAttribute("taskList", taskList);
+		RequestDispatcher rd = request.getRequestDispatcher("task-list.jsp");
+		rd.forward(request, response);
+		
 		
 		
 		
